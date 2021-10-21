d925f0a8ae8b59eaea0b572e34a994788e9f8c8d
Gordon <gordonpassy@gmail.com>
2021-10-21 22:21:28 +0300

    Add helper methods

--- src/main/kotlin/com/bcf/routes/service/RouteService.kt
+++ src/main/kotlin/com/bcf/routes/service/RouteService.kt
@@ -1,6 +1,8 @@
 package com.bcf.routes.service
 
+import com.bcf.routes.remote.client.Countries
 import com.bcf.routes.remote.client.CountriesAPIClient
+import com.bcf.routes.remote.client.CountriesItem
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -31,5 +33,42 @@ class RouteService: IRouteService {
 
         fun hasChildren(): Boolean =
             children.size >= 1
+    }
+
+    private fun mapBorders(root: Node<String>, mappedBorders: MutableSet<String>, destination: String, countries: Countries) {
+        mappedBorders.add(root.value)
+        val country: CountriesItem  = countries.first{ country -> country.cca3 == root.value }
+        val borders: List<String>?  = country.borders
+
+        if (borders.isNullOrEmpty())
+            return
+        else
+            borders.filter { border -> border !in mappedBorders }.forEach {
+                val child = Node(it)
+                root.addChild(child)
+            }
+
+        for (node in root.children) {
+            if (node.value == destination)
+                continue
+            mapBorders(node, mappedBorders, destination, countries)
+        }
+    }
+
+    private fun findAllRoutes(root: Node<String>, path: MutableList<String>, result: MutableList<List<String>>, destination: String): MutableList<List<String>> {
+        path.add(root.value)
+
+        if (!root.hasChildren()) {
+            if (path.contains(destination))
+                result.add(path.map { it })
+            path.removeLast()
+        } else {
+            for (child in root.children) {
+                findAllRoutes(root, path, result, destination)
+            }
+            path.removeLast()
+        }
+
+        return  result
     }
 }
