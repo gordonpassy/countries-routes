7db5f974745ee20e69e40b59155e05233e360f00
Gordon <gordonpassy@gmail.com>
2021-10-21 20:44:15 +0300

    Initial commit

---
+++ src/main/kotlin/com/bcf/routes/RoutesApplication.kt
@@ -0,0 +1,11 @@
+package com.bcf.routes
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RoutesApplication
+
+fun main(args: Array<String>) {
+    runApplication<RoutesApplication>(*args)
+}
